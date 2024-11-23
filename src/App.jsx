8a50fc7434317e5cd9167e8c55feb1ecf6fea3b0
sahildogra18 <sahildgr18@gmail.com>
2024-11-24 01:03:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [search, setSearch] = useState("");
+  let [contact, setContact] = useState([
+    { name: "sahil", phone: "8834547785" },
+    { name: "sudhanshu", phone: "7508393535" },
+    { name: "om", phone: "982544445" },
+    { name: "abhinav", phone: "8834547789" },
+    { name: "supriya", phone: "8834456787" },
+    { name: "Alexia", phone: "8834456787" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+
+  async function addkaro(e) {
+    e.preventDefault();
+
+    if (newName || newPhone) {
+      setContact([...contact, { name: newName, phone: newPhone }]);
+      let response = await fetch(
+        "https://contact-new-85f22-default-rtdb.firebaseio.com/contactreact.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(contact),
+        }
+      );
+    } else {
+      alert("enter valid values");
+    }
+  }
+
+  const filteredContacts = contact.filter((contact) => {
+    return contact.name.includes(search);
+  });
+  return (
+    <div className="conntainer">
+      <form method="POST">
+        <h1>Contact list</h1>
+
+        <div className="search">
+          <input
+            type="text"
+            placeholder="SERACH COONTACT HERE"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+        </div>
+        <h2 className="result-list"></h2>
+        {filteredContacts.map((c, index) => (
+          <li key={index}>
+            {c.name} -- {c.phone}
+          </li>
+        ))}
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your Name"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="number"
+            placeholder="Enter your Mobile Number"
+            value={newPhone}
+            onChange={(e) => setNewPhone(e.target.value)}
+          />
+        </div>
+
+        <button onClick={addkaro}>ADD </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
